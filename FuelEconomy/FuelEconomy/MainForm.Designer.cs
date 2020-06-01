379205fc6311e4dcfb1dbf820bf47a0a8333cfcd
Shaburov Ilya <shaburov.i@7gis.ru>
2020-06-01 23:16:23 +0500

    добавил инструкцию в программу

--- FuelEconomy/FuelEconomy/MainForm.Designer.cs
+++ FuelEconomy/FuelEconomy/MainForm.Designer.cs
@@ -51,6 +51,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cbPorts = new System.Windows.Forms.ComboBox();
             this.infoPage = new System.Windows.Forms.TabPage();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.tabControl.SuspendLayout();
@@ -312,6 +319,13 @@
             // 
             // infoPage
             // 
+            this.infoPage.Controls.Add(this.label13);
+            this.infoPage.Controls.Add(this.label12);
+            this.infoPage.Controls.Add(this.label11);
+            this.infoPage.Controls.Add(this.label10);
+            this.infoPage.Controls.Add(this.label9);
+            this.infoPage.Controls.Add(this.label8);
+            this.infoPage.Controls.Add(this.label6);
             this.infoPage.Controls.Add(this.label5);
             this.infoPage.Controls.Add(this.label4);
             this.infoPage.Location = new System.Drawing.Point(130, 4);
@@ -320,11 +334,89 @@
             this.infoPage.TabIndex = 2;
             this.infoPage.Text = "Инфо";
             // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(3, 230);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(461, 15);
+            this.label13.TabIndex = 8;
+            this.label13.Text = "Дополнительно можете указать производительность форсунок автомобиля.";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(3, 260);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(340, 15);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "6) На экране \"Устройство\" нажмите кнопку \"Подключить\"";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(3, 200);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(425, 15);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "5) На экране \"Настройки\" выберите порт к которому подключен сканер.";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(3, 170);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(446, 15);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "4) Если потребуется ввод пароля, попробуйте стандартные 1234 или 0000.";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(3, 140);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(492, 15);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "3) Средствами Windows произвести установить Bluetooth соединение со сканером.";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(3, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(418, 15);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "2) Подключить к компьютеру Bluetooth адаптер, установить драйвера.";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(502, 15);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "1) Подключить диагностический OBD-II Bluetooth сканер ELM 327 v.1.5 к автомобилю." +
+    "";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(3, 42);
+            this.label5.Location = new System.Drawing.Point(3, 50);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(478, 15);
             this.label5.TabIndex = 1;
@@ -388,6 +480,13 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label rpmDashboard;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
     }
 }
 
